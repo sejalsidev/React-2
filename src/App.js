0e9334ca74856a7a116e681c2cb33f6ed81dcfc6
sejal dev SI <sejalsidev001@gmail.com>
2024-05-03 17:39:13 +0530

    Login Add-Update-Delete-Data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-
 import "./App.css";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import AddProduct from "./AddProduct";
 import UpdateProduct from "./UpdateProduct";
-import Login from "./Login";
+import Login from "./Component/Login";
 import Register from "./Register";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Get from "./Component.js/Get";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Get from "./Component/Get";
 import PostForm from "./PostForm";
+import Counter from "./Counter";
+import HookCounter from "./HookCounter";
+import HookCounterTwo from "./HookCounterTwo";
+import HookObject from "./HookObject";
+import HookCounterFour from "./HookCounterFour";
+import HookCounterEffectDemo from "./HookCounterEffectDemo";
+import DataFetching from "./DataFetching";
 function App() {
   return (
     <div className="App">
-      
-      {/* <Get/> */}
-      <PostForm/>
+      <Login/>
+      {/*   <HookCounterFour/> */}
+      {/*   <Counter/> */}
+      {/* <HookCounter/> */}
+      {/*   <HookCounterTwo/> */}
+      {/*  <HookObject/> */}
 
-     {/*  <BrowserRouter>
+      {/* <Get/> */}
+      {/*   <PostForm/>
+       */}
+      {/* <HookCounterEffectDemo/> */}
+    {/*   <DataFetching/> */}
+
+      {/*  <BrowserRouter>
      
         
           <Routes>
